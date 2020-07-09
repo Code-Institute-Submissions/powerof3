b6120b60d148655c2f2e2f5e16434efbde6a166c
rcesonis <rimvydas.cesonis@gmail.com>
2020-07-09 17:14:04 +0200

    set core views

--- po3/__init__.py
+++ po3/__init__.py
@@ -1,3 +1,16 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+
+##########################
+# DATABASE SETTINGS ######
+##########################
+
+
+
+##########################
+# BLUEPRINTS #############
+##########################
+from po3.core.views import core
+app.register_blueprint(core)
--- po3/core/views.py
+++ po3/core/views.py
@@ -1,6 +1,12 @@
-from flask import render_template
+from flask import render_template,Blueprint,request,url_for
+
+core = Blueprint('core',__name__)
 
 
 @core.route('/')
 def index():
     return render_template('index.html')
+
+@core.route('/about')
+def about():
+    return render_template('about.html')
