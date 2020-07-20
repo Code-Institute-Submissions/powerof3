79ece4c863e00fdf59d3a4690b0a01d1075ee76d
rcesonis <rimvydas.cesonis@gmail.com>
2020-07-20 15:33:18 +0200

    Finished recipe editing functiont

--- po3/recipes/views.py
+++ po3/recipes/views.py
@@ -41,7 +41,7 @@ def recipe_card(recipe_post_id):
 
 # Edit
 
-@recipe_card('/<int:recipe_card_id/edit',methods=['GET','POST'])
+@recipe_card.route('/<int:recipe_card_id/edit',methods=['GET','POST'])
 @login_required
 def edit(recipe_card_id):
     recipe_card = Recipe.query.get_or_404(recipe_card_id)
@@ -53,18 +53,27 @@ def edit(recipe_card_id):
 
     if form.validate_on_submit():
 
-        recipe_card.title = form.title.data,
-        recipe_card.description = form.description.data,
-        recipe_card.ingriedient1 = form.ingriedient1.data,
-        recipe_card.ingriedient2 = form.ingriedient2.data,
-        recipe_card.ingriedient3 = form.ingriedient3.data,
-        recipe_card.smoothie_image = form.smoothie_image.data,
+        recipe_card.title = form.title.data
+        recipe_card.description = form.description.data
+        recipe_card.ingriedient1 = form.ingriedient1.data
+        recipe_card.ingriedient2 = form.ingriedient2.data
+        recipe_card.ingriedient3 = form.ingriedient3.data
+        recipe_card.smoothie_image = form.smoothie_image.data
         recipe_card.user_id = current_user.id
 
-        db.session.add(recipe_card)
         db.session.commit()
         flash('Recipe Card updated')
-        return redirect(url_for('recipe_card.index'))
+        return redirect(url_for('recipe_cards.recipe_card',recipe_card_id=recipe_card.id))
+    
+    elif request.method = "GET":
+        form.title.data = recipe_card.title
+        form.description.data = recipe_card.description
+        form.ingriedient1.data = recipe_card.ingriedient1
+        form.ingriedient2.data = recipe_card.ingriedient2
+        form.ingriedient3.data = recipe_card.ingriedient3
+        form.smoothie_image.data = recipe_card.smoothie_image
+
+    return render_template('add_recipe.html',title='Editing',form=form)
 
 
 
